b0f6e72b560b0f3ebfc040685653736304cfb583
DinoCzar <116979569+DinoCzar@users.noreply.github.com>
2023-01-18 14:40:43 -0800

    Rearrange buttons on calculator

--- script.js
+++ script.js
@@ -28,7 +28,66 @@ const operator = function (firstNumber, operator, secondNumber) {
     }
 }
 
-for (let i = 0; i < 10; i++) {
+const left = document.querySelector('#left');
+const right = document.querySelector('#right');
+
+const zero = document.createElement('button');
+zero.setAttribute('id', 'numbers');
+zero.textContent = '0';
+zero.addEventListener('click', () => {
+    console.log(0);
+});
+left.appendChild(zero);
+
+const clear = document.createElement('button');
+clear.setAttribute('id', 'numbers');
+clear.textContent = 'C';
+clear.addEventListener('click', () => {
+    console.log('clear');
+});
+left.appendChild(clear);
+
+const equals = document.createElement('button');
+equals.setAttribute('id', 'numbers');
+equals.textContent = '=';
+equals.addEventListener('click', () => {
+    console.log('=');
+});
+left.appendChild(equals);
+
+const addition = document.createElement('button');
+addition.setAttribute('id', 'numbers');
+addition.textContent = '+';
+addition.addEventListener('click', () => {
+    console.log('+');
+});
+right.appendChild(addition);
+
+const subtraction = document.createElement('button');
+subtraction.setAttribute('id', 'numbers');
+subtraction.textContent = '-';
+subtraction.addEventListener('click', () => {
+    console.log('-');
+});
+right.appendChild(subtraction);
+
+const multiplication = document.createElement('button');
+multiplication.setAttribute('id', 'numbers');
+multiplication.textContent = 'x';
+multiplication.addEventListener('click', () => {
+    console.log('*');
+});
+right.appendChild(multiplication);
+
+const division = document.createElement('button');
+division.setAttribute('id', 'numbers');
+division.textContent = '/';
+equals.addEventListener('click', () => {
+    console.log('/');
+});
+right.appendChild(division);
+
+for (let i = 1; i < 10; i++) {
     const left = document.querySelector('#left');
     const numbers = document.createElement('button');
     numbers.setAttribute('id', 'numbers');
@@ -39,41 +98,3 @@ for (let i = 0; i < 10; i++) {
     left.appendChild(numbers);
 }
 
-const left = document.querySelector('#left');
-const right = document.querySelector('#right');
-
-const equals = document.createElement('button');
-equals.setAttribute('id', 'numbers');
-equals.textContent = '=';
-left.appendChild(equals);
-
-const clear = document.createElement('button');
-clear.setAttribute('id', 'numbers');
-clear.textContent = 'C';
-left.appendChild(clear);
-
-const addition = document.createElement('button');
-addition.setAttribute('id', 'numbers');
-addition.textContent = '+';
-right.appendChild(addition);
-
-const subtraction = document.createElement('button');
-subtraction.setAttribute('id', 'numbers');
-subtraction.textContent = '-';
-right.appendChild(subtraction);
-
-const multiplication = document.createElement('button');
-multiplication.setAttribute('id', 'numbers');
-multiplication.textContent = '+';
-right.appendChild(multiplication);
-
-const division = document.createElement('button');
-division.setAttribute('id', 'numbers');
-division.textContent = '+';
-right.appendChild(division);
-
-
-
-
-
-console.log(operator(10, '+', 10));
