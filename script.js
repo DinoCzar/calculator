42e504551051707aca94ba806b6f7838865b0740
DinoCzar <116979569+DinoCzar@users.noreply.github.com>
2023-01-18 14:21:53 -0800

    Create calculator visual in style.css, add buttons to calculator in script.js, add divs in index.html, return number values for buttons in script.js

--- script.js
+++ script.js
@@ -28,4 +28,52 @@ const operator = function (firstNumber, operator, secondNumber) {
     }
 }
 
+for (let i = 0; i < 10; i++) {
+    const left = document.querySelector('#left');
+    const numbers = document.createElement('button');
+    numbers.setAttribute('id', 'numbers');
+    numbers.textContent = [i];
+    numbers.addEventListener('click', () => {
+        console.log(i);
+    });
+    left.appendChild(numbers);
+}
+
+const left = document.querySelector('#left');
+const right = document.querySelector('#right');
+
+const equals = document.createElement('button');
+equals.setAttribute('id', 'numbers');
+equals.textContent = '=';
+left.appendChild(equals);
+
+const clear = document.createElement('button');
+clear.setAttribute('id', 'numbers');
+clear.textContent = 'C';
+left.appendChild(clear);
+
+const addition = document.createElement('button');
+addition.setAttribute('id', 'numbers');
+addition.textContent = '+';
+right.appendChild(addition);
+
+const subtraction = document.createElement('button');
+subtraction.setAttribute('id', 'numbers');
+subtraction.textContent = '-';
+right.appendChild(subtraction);
+
+const multiplication = document.createElement('button');
+multiplication.setAttribute('id', 'numbers');
+multiplication.textContent = '+';
+right.appendChild(multiplication);
+
+const division = document.createElement('button');
+division.setAttribute('id', 'numbers');
+division.textContent = '+';
+right.appendChild(division);
+
+
+
+
+
 console.log(operator(10, '+', 10));
