fecdc7f149d6e56b01893628227b8a7a789ee464
DinoCzar <116979569+DinoCzar@users.noreply.github.com>
2023-01-18 11:07:41 -0800

    Add operator function

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ const multiply = function (a , b) {
 const divide = function (a, b) {
     return a / b;
 };
+
+const operator = function (firstNumber, operator, secondNumber) {
+    if (operator === '+') {
+        return add(firstNumber, secondNumber);
+    } else if (operator === '-') {
+        return subtract(firstNumber, secondNumber);
+    } else if (operator === '*') {
+        return multiply(firstNumber, secondNumber);
+    } else if (operator === '/') {
+        return divide(firstNumber, secondNumber);
+    } else {
+        return alert("Please enter a valid operation");
+    }
+}
+
+console.log(operator(10, '+', 10));
