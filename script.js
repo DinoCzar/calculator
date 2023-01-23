7f1a20e593cb8ba4b06fb83d192a021c24e66b06
DinoCzar <116979569+DinoCzar@users.noreply.github.com>
2023-01-23 02:44:08 -0800

    Add function to process and display result when user presses the equals button

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const display = document.querySelector('#display');
-const displayContent = document.createElement('div');
+let display = document.querySelector('#display');
+let displayContent = document.createElement('div');
 displayContent.setAttribute('id', 'displayContent');
 display.appendChild(displayContent);
 
 let clear = document.querySelector('#clear');
 let equals = document.querySelector('#equals');
-
 let numbers = document.querySelectorAll('.number');
 let operators = document.querySelectorAll('.operator');
 
@@ -18,16 +17,16 @@ numbers.forEach((number) => number.addEventListener('click', function(e) {
     displayContent.textContent = currentValue;
 }));
 
-operators.forEach((op) => op.addEventListener('click', function(e) {
-    handleOperator(e.target.textContent);
+operators.forEach((currentOperator) => currentOperator.addEventListener('click', function(e) {
+    operation(e.target.textContent);
     displayContent.textContent = previousValue;
-}))
+}));
 
-function handleNumber(num) {
-    currentValue += num;
-}
+function handleNumber(number) {
+    currentValue += number;
+};
 
-function handleOperator(op) {
+function operation(currentOperator) {
     if (previousValue === '') {
         previousValue = 0;
     } else {
@@ -40,8 +39,32 @@ function handleOperator(op) {
 
     currentValue = Number(currentValue);
 
-console.log('previous value: ' + previousValue)
-console.log('current value: ' + currentValue)
+    if (previousOperator === '+') {
+        previousValue += currentValue;
+    } else if (previousOperator === '-') {
+        previousValue -= currentValue;
+    } else if (previousOperator === 'x') {
+        previousValue *= currentValue;
+    } else {
+        previousValue /= currentValue;
+    }
+
+    currentValue = '';
+    previousOperator = currentOperator;
+};
+
+equals.addEventListener('click', function() {
+    if (previousValue === '') {
+        previousValue = 0;
+    } else {
+        Number(previousValue);
+    };
+
+    if (previousOperator === '') {
+        previousOperator = '+';
+    };
+
+    currentValue = Number(currentValue);
 
     if (previousOperator === '+') {
         previousValue += currentValue;
@@ -54,16 +77,8 @@ console.log('current value: ' + currentValue)
     }
 
     currentValue = '';
-console.log('previous value: ' + previousValue)
-console.log('current value: ' + currentValue)
-console.log('previous operator: ' + previousOperator)
-
-    previousOperator = op;
-
-    console.log('previous operator: ' + previousOperator)
-
-
-}
+    displayContent.textContent = previousValue;
+});
 
 clear.addEventListener('click', function() {
     previousValue = '';
@@ -71,98 +86,3 @@ clear.addEventListener('click', function() {
     operator = '';
     displayContent.textContent = currentValue;
 });
-
-equals.addEventListener('click', function() {
-    calculate()
-    displayContent.textContent = previousValue;
-})
-
-function calculate() {
-    previousValue = Number(previousValue);
-    currentValue = Number(currentValue);
-
-    if (operator === '+') {
-        previousValue += currentValue;
-    } else if (operator === '-') {
-        previousValue -= currentValue;
-    } else if (operator === 'x') {
-        previousValue *= currentValue;
-    } else {
-        previousValue /= currentValue;
-    }
-
-    previousValue = previousValue.toString();
-    currentValue = currentValue.toString();
-}
-
-
-
-
-/*
-
-const add = function (a, b) {
-    return a + b;
-};
-
-const subtract = function (a ,b) {
-    return a - b;
-};
-
-const multiply = function (a , b) {
-    return a * b;
-};
-
-const divide = function (a, b) {
-    return a / b;
-};
-
-const operation = function (firstNumber, operator, secondNumber) {
-    if ((operator === '/') && (secondNumber === 0)) {
-        return 'Not so fast';
-    } else if (operator === '+') {
-        return add(firstNumber, secondNumber);
-    } else if (operator === '-') {
-        return subtract(firstNumber, secondNumber);
-    } else if (operator === '*') {
-        return multiply(firstNumber, secondNumber);
-    } else if (operator === '/') {
-        return divide(firstNumber, secondNumber);
-    }
-};
-
-const display = document.querySelector('#display');
-const displayContent = document.createElement('div');
-displayContent.setAttribute('id', 'displayContent');
-display.appendChild(displayContent);
-
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => button.addEventListener('click', () => calculate(button)));
-
-function calculate(button) {
-
-    displayContent.textContent = (displayContent.textContent += button.textContent);
-    displayValue = displayContent.textContent;
-
-    if (button.textContent === '=') {
-
-        if(displayValue.includes('+')) {
-            var operator = '+';
-        } else if (displayValue.includes('-')) {
-            var operator = '-';
-        } else if (displayValue.includes('*')) {
-            var operator = '*';
-        } else if (displayValue.includes('/')) {
-            var operator = '/';
-        };
-
-        var firstNumber = Number(displayValue.substring(0, (displayValue.indexOf(operator))));
-        var secondNumber = Number(displayValue.substring(displayValue.indexOf(operator) + 1).slice(0,-1));
-        displayContent.textContent = operation(firstNumber, operator, secondNumber);
-    };
-
-    if (button.textContent === 'C') {
-        displayContent.textContent = '';
-    };
-};
-
-*/
